cacd9c771ae6c21089996518ff5cbee2dcd5da99
Nahoj_G <johan@granados.com.co>
2021-11-29 06:31:15 -0500

    Entrega semana 5

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -1,26 +1,21 @@
 package main;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class MainMenu {
 
     public static final int WIDTH = 1514,  HEIGHT = 1080;
-    private JFrame frame;
-    private ImageIcon animalIcon,windowIcon;
-    private JLabel labelBackground;  
-    private LBotton startButton,exitButton;
-    Sound music = new Sound();   
-    //ClassLoader cl=getClass().getClassLoader();
+    private final JFrame frame;
+    Sound music = new Sound();
     
     public MainMenu() {
     	
     	System.out.println("Inicializando juego");
     	System.out.println("Animal Pop IT");
-        animalIcon = new ImageIcon(this.getClass().getResource("/images/index.png"));     
-        windowIcon = new ImageIcon(this.getClass().getResource("/images/icon.png")); 
-        labelBackground = new JLabel(animalIcon);
+        ImageIcon animalIcon = new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/images/index.png")));
+        ImageIcon windowIcon = new ImageIcon(Objects.requireNonNull(this.getClass().getResource("/images/icon.png")));
+        JLabel labelBackground = new JLabel(animalIcon);
         labelBackground.setSize(WIDTH,HEIGHT);
 
         frame = new JFrame("Animal Pop It");      
@@ -30,28 +25,21 @@ public class MainMenu {
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        frame.setIconImage(windowIcon.getImage());        
-   
-        startButton = new LBotton("start",622,618,270,100);        
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-             //new CountForm(true);
-               frame.setVisible(false);
-               new EntryUser();
-               music.stop();
-               frame.dispose();
-            }
+        frame.setIconImage(windowIcon.getImage());
+
+        LBotton startButton = new LBotton("start", 622, 618, 270, 100);
+        startButton.addActionListener(e -> {
+           frame.setVisible(false);
+           new EntryUser();
+           music.stop();
+           frame.dispose();
         });
-        
-        exitButton = new LBotton("exit",622,736,270,100);   
-        exitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {             
-               frame.dispose();
-               music.stop();
-               System.exit(0);
-            }
+
+        LBotton exitButton = new LBotton("exit", 622, 736, 270, 100);
+        exitButton.addActionListener(e -> {
+           frame.dispose();
+           music.stop();
+           System.exit(0);
         });
                
        labelBackground.add(startButton);
